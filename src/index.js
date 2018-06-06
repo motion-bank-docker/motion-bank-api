ac9aacf951b39ab7f6f30d4f40187d58300c755c
A. Koch <dasantonym@users.noreply.github.com>
2018-06-06 15:27:16 +0200

    dded config, basic resource service, models and persistence added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const
+  config = require('config'),
   http = require('http'),
   polka = require('polka'),
-  { json } = require('body-parser'),
-  Primus = require('primus')
+  Primus = require('primus'),
+  morgan = require('morgan'),
+  { json } = require('body-parser')
 
 /**
  * HTTP server
  */
 const
-  { PORT = 3030, HOST = '0.0.0.0' } = process.env,
+  host = config.get('http.host'),
+  port = config.get('http.port'),
   server = http.createServer()
 
 /**
@@ -35,18 +38,20 @@ primus.on('disconnection', spark => {
  * Setup API server (Polka)
  */
 const
+  models = require('mbjs-data-models'),
   Service = require('./service'),
   app = polka({ server })
 
 app.use(json())
+app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'))
 
 /**
  * Configure resources
  */
-const annotations = new Service('annotations', app)
+const annotations = new Service('annotations', app, models.Annotation, winston)
 annotations.on('message', message => primus.write(message))
 
 /**
  * Start server
  */
-app.listen(PORT, HOST).then(() => winston.log('info', `API started on ${HOST}:${PORT}`))
+app.listen(port, host).then(() => winston.log('info', `API started on ${host}:${port}`))
--- src/service.js
+++ src/service.js
@@ -1,47 +1,96 @@
 const
+  config = require('config'),
   send = require('@polka/send-type'),
-  TinyEmitter = require('tiny-emitter')
+  TinyEmitter = require('tiny-emitter'),
+  { ObjectUtil } = require('mbjs-utils'),
+  { MongoDB } = require('mbjs-persistence')
 
 class Service extends TinyEmitter {
-  constructor (name, app) {
+  constructor (name, app, model, logger) {
     super()
 
     this._name = name
+    this._Model = model
+    // TODO: make db adapter configurable (nedb, etc.)
+    this._client = new MongoDB(ObjectUtil.merge({ name, logger }, config.get('resources.mongodb')), 'uuid')
 
     const _this = this
 
-    app.get(`/${this._name}.json`, (req, res) => {
-      _this._response(req, res, [{ hello: 'world' }])
+    app.get(`/${this._name}.json`, async (req, res) => {
+      const results = await this.client.find(req.params.query || {}, req.params)
+      _this._response(req, res, results)
     })
 
-    app.get(`/${this._name}/:id.json`, (req, res) => {
-      _this._response(req, res, { hello: 'world' })
+    app.get(`/${this._name}/:id.json`, async (req, res) => {
+      const result = await this.client.get(req.params.id, req.params)
+      if (result) {
+        const instance = new this.ModelConstructor(result, `${req.params.id}`)
+        return _this._response(req, res, instance)
+      }
+      send(res, 404)
     })
 
-    app.post(`/${this._name}.json`, (req, res) => {
+    app.post(`/${this._name}.json`, async (req, res) => {
+      const
+        ctx = this,
+        data = req.body
+      if (Array.isArray(data)) {
+        const results = await Promise.all(data.map(entry => {
+          return ctx.create(entry, req.params)
+        }))
+        return _this._response(req, res, results)
+      }
+      // TODO: allow for full array inserts instead just single requests
+      const instance = new this.ModelConstructor(data),
+        result = await this.client.create(instance, req.params)
+      instance.update(result)
+      _this._response(req, res, instance)
+    })
+
+    app.put(`/${this._name}/:id.json`, async (req, res) => {
       const data = req.body
-      _this._response(req, res, data)
+      let result = await this.get(req.params.id)
+      if (result) {
+        // TODO: transactions anyone?!
+        let instance = new this.ModelConstructor(data, req.params.id)
+        result = await this.client.update(req.params.id, instance, req.params)
+        instance = new this.ModelConstructor(result, req.params.id)
+        return _this._response(req, res, instance)
+      }
+      send(res, 404)
     })
 
-    app.put(`/${this._name}/:id.json`, (req, res) => {
+    app.patch(`/${this._name}/:id.json`, async (req, res) => {
       const data = req.body
-      _this._response(req, res, data)
+      let instance = await this.get(req.params.id)
+      if (instance) {
+        instance.update(data)
+        await this.client.update(req.params.id, instance, req.params)
+        return _this._response(req, res, instance)
+      }
+      send(res, 404)
     })
 
-    app.patch(`/${this._name}/:id.json`, (req, res) => {
-      const data = req.body
-      _this._response(req, res, data)
-    })
-
-    app.delete(`/${this._name}/:id.json`, (req, res) => {
-      const data = req.body
-      _this._response(req, res, data)
+    app.delete(`/${this._name}/:id.json`, async (req, res) => {
+      const result = await this.client.remove(req.params.id, req.params)
+      if (result) {
+        return _this._response(req, res, result)
+      }
+      send(res, 404)
     })
   }
 
   _response (req, res, data = {}) {
     this.emit('message', { method: req.method, id: data.id })
     send(res, 200, data)
+  }
+
+  get client () {
+    return this._client
+  }
+
+  get ModelConstructor () {
+    return this._Model
   }
 }
 
