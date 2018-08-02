3c8a0e00a1665f731211ba4343712be57730ced8
A. Koch <dasantonym@users.noreply.github.com>
2018-08-02 15:18:07 +0200

    refactored, now using generic api module

--- src/index.js
+++ src/index.js
@@ -1,141 +1,62 @@
-const
-  config = require('config'),
-  path = require('path'),
-  http = require('http'),
-  polka = require('polka'),
-  Primus = require('primus'),
-  morgan = require('morgan'),
-  cors = require('cors')({ origin: true }),
-  send = require('@polka/send-type'),
-  jwt = require('express-jwt'),
-  favicon = require('serve-favicon'),
-  jwks = require('jwks-rsa'),
-  { json } = require('body-parser'),
-  { ObjectUtil } = require('mbjs-utils')
+const GenericAPI = require('mbjs-generic-api')
 
 const setup = async function () {
-  /**
-   * HTTP server
-   */
-  const
-    host = config.get('http.host'),
-    port = config.get('http.port'),
-    server = http.createServer()
-
-  /**
-   * Logger
-   */
-  const winston = require('winston')
-  winston.level = process.env.NODE_ENV === 'production' ? 'error' : 'info'
-  winston.level = process.env.LOG_LEVEL || winston.level
-
-  /**
-   * Set up WebSockets (Primus)
-   */
-  const primus = new Primus(server, { transformer: 'uws' })
-
-  primus.on('connection', spark => {
-    winston.log('debug', `Spark ${spark.id} connected with address ${spark.address}`)
-  })
-
-  primus.on('disconnection', spark => {
-    winston.log('debug', `Spark ${spark.id} disconnected with address ${spark.address}`)
-  })
-
-  /**
-   * Setup API server (Polka)
-   */
-  const
-    app = polka({
-      server,
-      onError (err, req, res, next) {
-        send(res, err.status || 500, { code: err.code })
-      }
-    })
-
-  app.use(favicon(path.join(__dirname, '..', 'assets', 'favicon.ico')))
-
-  const jwtCheck = jwt(ObjectUtil.merge({
-    secret: jwks.expressJwtSecret(config.get('auth.jwks'))
-  }, config.get('auth.jwt')))
-
-  app.use(cors, jwtCheck, json(), morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev'))
-
-  /**
-   * Add proxy
-   */
-  const proxy = require('./middleware/proxy')
-  proxy(app)
-
-  /**
-   * Set up ACL
-   */
-  const
-    setupACL = require('./auth/acl'),
-    acl = await setupACL(app)
-
-  /**
-   * Middleware
-   */
-  const addUserUUID = require('./middleware/user')
-  addUserUUID(app)
+  const api = new GenericAPI()
+  await api.setup()
 
   /**
    * Configure Profiles
    */
   const
-    Profiles = require('./profiles'),
-    profiles = new Profiles(app)
-  profiles.on('message', message => winston.debug(message))
+    Profiles = require('./lib/profiles'),
+    profiles = new Profiles(api._app)
+  // profiles.on('message', message => api._logger.debug(message))
 
   const addAuthor = require('./middleware/author')
-  addAuthor(app, profiles)
+  addAuthor(api._app, profiles)
 
   /**
    * Configure resources
    */
   const
     models = require('mbjs-data-models'),
-    Service = require('./service')
+    Service = require('./lib/service')
 
-  const annotations = new Service('annotations', app, models.Annotation, winston, acl)
-  annotations.on('message', message => primus.write(message))
+  const annotations = new Service('annotations', api._app, models.Annotation, api._logger, api._acl)
+  // annotations.on('message', message => api._sockets.write(message))
 
-  const maps = new Service('maps', app, models.Map, winston, acl)
-  maps.on('message', message => primus.write(message))
+  const maps = new Service('maps', api._app, models.Map, api._logger, api._acl)
+  // maps.on('message', message => api._sockets.write(message))
 
-  const documents = new Service('documents', app, models.Document, winston, acl)
-  documents.on('message', message => primus.write(message))
+  // const documents = new Service('documents', api._app, models.Document, api._logger, api._acl)
+  // documents.on('message', message => api._sockets.write(message))
 
   /**
    * Configure metadata
    */
 
   const
-    Metadata = require('./metadata'),
-    metadata = new Metadata(app, annotations)
-  metadata.on('message', message => primus.write(message))
+    Metadata = require('./lib/metadata'),
+    metadata = new Metadata(api._app, annotations)
+  // metadata.on('message', message => api._sockets.write(message))
 
   /**
    * Configure sessions
    */
 
   const
-    Sessions = require('./sessions'),
-    sessions = new Sessions(app, maps, annotations)
-  sessions.on('message', message => primus.write(message))
+    Sessions = require('./lib/sessions'),
+    sessions = new Sessions(api._app, maps, annotations)
+  // sessions.on('message', message => api._logger.write(message))
 
   /**
    * Configure archives
    */
 
-  const archives = require('./archives')
-  archives.setupArchives(app, maps, annotations)
+  const archives = require('./lib/archives')
+  archives.setupArchives(api._app, maps, annotations)
 
-  /**
-   * Start server
-   */
-  return app.listen(port, host).then(() => winston.info(`API started on ${host}:${port}`))
+  await api.start()
 }
 
 setup().catch(err => {
