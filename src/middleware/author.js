c238cb455b374d3ee78e6174a9470baefaa3c2ed
A. Koch <dasantonym@users.noreply.github.com>
2018-08-21 16:20:09 +0200

    only add author if user exists

--- src/middleware/author.js
+++ src/middleware/author.js
@@ -1,16 +1,18 @@
 const setup = async function (api, profileService) {
   api.app.use(async (req, res, next) => {
-    const r = {
-      params: {
-        id: req.user.uuid
-      },
-      user: req.user
-    }
-    req.user.profile = await profileService.getHandler(r)
-    if (req.method.toLowerCase() === 'post') {
-      req.body.author = {
-        id: req.user.uuid,
-        name: req.user.profile ? req.user.profile.name : undefined
+    if (req.user) {
+      const r = {
+        params: {
+          id: req.user.uuid
+        },
+        user: req.user
+      }
+      req.user.profile = await profileService.getHandler(r)
+      if (req.method.toLowerCase() === 'post') {
+        req.body.author = {
+          id: req.user.uuid,
+          name: req.user.profile ? req.user.profile.name : undefined
+        }
       }
     }
     next()
