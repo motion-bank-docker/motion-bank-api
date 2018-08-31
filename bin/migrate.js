cdb93e3319576b441e58bc979d6b97d82e26e56b
A. Koch <dasantonym@users.noreply.github.com>
2018-08-31 15:32:38 +0200

    update data model dependency, update migrate script

--- bin/migrate.js
+++ bin/migrate.js
@@ -20,12 +20,15 @@ const proc = async function () {
   await mapsClient.connect()
   const maps = await mapsClient.find({})
   for (let map of maps) {
+    Object.keys(map).forEach(key => {
+      if (key[0] === '_') map[key] = undefined
+    })
     if (typeof map.author === 'string') {
       map.author = { id: map.author }
       console.log('updating author to', map.author)
     }
     const mi = new Map(map)
-    await mapsClient.update(map.uuid, mi)
+    await mapsClient.update(mi.uuid, mi.toObject(), {})
   }
 
   console.log('ANNOTATIONS\n-------------------\n\n')
@@ -37,6 +40,9 @@ const proc = async function () {
   await annoClient.connect()
   const annos = await annoClient.find({})
   for (let anno of annos) {
+    Object.keys(anno).forEach(key => {
+      if (key[0] === '_') anno[key] = undefined
+    })
     if (typeof anno.author === 'string') {
       anno.author = { id: anno.author }
       console.log('updating author to', anno.author)
@@ -49,6 +55,10 @@ const proc = async function () {
       anno.target.id = anno.target.id.replace(gridPrefix, `${newPrefix}maps/`)
       console.log('updating grid target to', anno.target.id)
     }
+    if (anno.target.type === 'Video' && typeof anno.target.id === 'string' && uuid.isUUID(anno.target.id)) {
+      anno.target.id = `${newPrefix}annotations/${anno.target.id}`
+      console.log('updating video target to', anno.target.id)
+    }
     if (anno.target.type === 'Timeline' && typeof anno.target.id === 'string' && uuid.isUUID(anno.target.id)) {
       anno.target.id = `${newPrefix}maps/${anno.target.id}`
       console.log('updating timeline target to', anno.target.id)
@@ -58,7 +68,7 @@ const proc = async function () {
       console.log('updating grid target to', anno.target.id)
     }
     const ai = new Annotation(anno)
-    await mapsClient.update(anno.uuid, ai)
+    await annoClient.update(ai.uuid, ai.toObject(), {})
   }
 }
 
