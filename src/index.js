989c8675feee391fa38b6d3b7658f02d7d7aa897
Anton <antonymousfeedback@gmail.com>
2020-02-13 15:17:48 +0100

    Remove PBA related code

--- src/index.js
+++ src/index.js
@@ -55,14 +55,6 @@ const setup = async function () {
   const archives = require('./lib/archives')
   archives.setupArchives(api, maps, annotations, cells)
 
-  /**
-   * Configure PBA
-   */
-
-  const
-    PBA = require('./lib/pba'),
-    pba = new PBA(api)
-
   await api.start()
 }
 
