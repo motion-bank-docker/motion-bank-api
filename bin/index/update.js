8e54044f3e903f8188b9f182d1cb5db7d3d00f6e
anton <dasantonym@users.noreply.github.com>
2020-06-25 18:40:51 +0200

    Add index/update for shell

--- bin/index/update.js
+++ bin/index/update.js
@@ -2,14 +2,30 @@
 
 const
   config = require('config'),
-  SearchIndex = require('mbjs-generic-api/src/lib/search-index')
+  { ObjectUtil } = require('mbjs-utils'),
+  { MongoDB } = require('mbjs-persistence'),
+  SearchIndex = require('mbjs-generic-api/src/lib/search-index'),
+  searchIndexMappings = require('../../src/search-index-mappings')
 
 const main = async function () {
-  const [node, cmd, index] = process.argv
+  const [node, cmd, index, force] = process.argv
   process.stdout.write(`${node} ${cmd}\n`)
   process.stdout.write(`Updating index ${index}...\n`)
 
-  // const index = new SearchIndex(index)
+  const searchIndex = new SearchIndex(index, searchIndexMappings[index])
+  await searchIndex.ensure(!!force)
+
+  let count = 0
+  const client = new MongoDB(ObjectUtil.merge({name: index, logger: console}, config.get('resources.mongodb')), 'id')
+  const results = await client.find({})
+  for (const item of results) {
+    await searchIndex.index(item.id, ObjectUtil.merge({}, item, {_id: undefined}))
+    count++
+    if (count % 100 === 0) process.stdout.write(`Items ${count}\n`)
+  }
+
+  process.stdout.write(`Items ${count}\n`)
+  process.exit(0)
 }
 
 main()
