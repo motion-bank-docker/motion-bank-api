86c5248e38a2d280e2030f58743cf88f6369b2f4
anton <dasantonym@users.noreply.github.com>
2020-06-26 17:42:11 +0200

    Update search-index-mappings.js

--- src/search-index-mappings.js
+++ src/search-index-mappings.js
@@ -1,8 +1,32 @@
 const searchIndexMappings = {
   annotations: {
     properties: {
+      id: {
+        type: 'keyword'
+      },
+      type: {
+        type: 'keyword'
+      },
+      'body.type': {
+        type: 'keyword'
+      },
+      'body.source.id': {
+        type: 'keyword'
+      },
+      'body.source.type': {
+        type: 'keyword'
+      },
+      'target.id': {
+        type: 'keyword'
+      },
+      'target.selector.id': {
+        type: 'keyword'
+      },
       'target.selector._valueMillis': {
         type: 'date'
+      },
+      'creator.id': {
+        type: 'keyword'
       },
       _created: {
         type: 'date'
@@ -14,6 +38,15 @@ const searchIndexMappings = {
   },
   maps: {
     properties: {
+      id: {
+        type: 'keyword'
+      },
+      type: {
+        type: 'keyword'
+      },
+      'creator.id': {
+        type: 'keyword'
+      },
       _created: {
         type: 'date'
       },
